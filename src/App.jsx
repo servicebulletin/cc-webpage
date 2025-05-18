0e36982439832cd5d8541f0703060ef3358517a2
servicebulletin <36870168+servicebulletin@users.noreply.github.com>
2025-05-18 16:26:32 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,25 @@ function App() {
         <h1>CRAWFORD&CORRY</h1>
         <h4>EMPLOYABLE | EDUCATIONAL | EXPERIENTIAL</h4>
       </div>
-      
+      <div>
+        <h3>
+          With deep expertise, unique access, and extensive resources, we specialise in 
+          designing bespoke, secure, and transformative professional placements and work 
+          experiences in Vietnam for young adults, available year-round, including summer 
+          and gap year programmes.
+        </h3>
+        <p>
+        Our programmes emphasise career development, cultural immersion, and personal 
+        enrichment, all curated to help each participant achieve their goals and fulfil 
+        their potential. Safety, security, and privacy are our utmost priorities, forming 
+        the cornerstone of our approach. Discretion is integral to everything we do, 
+        particularly as we work with students and young adults. In keeping with our 
+        commitment to exclusivity and trust, we maintain a deliberately minimal public 
+        presence and refrain from social media activity. Crawford & Corry was founded by 
+        two professionals with backgrounds at Eton and Oxford, combined with extensive 
+        on-the-ground experience in Vietnam.
+        </p>
+      </div>
     </>
   )
 }
