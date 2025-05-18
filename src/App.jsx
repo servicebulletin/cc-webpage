befe128a1f0f520017e1c1841350802575fed73b
servicebulletin <36870168+servicebulletin@users.noreply.github.com>
2025-05-18 18:38:20 +0800

    Add brochure download

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,9 @@ function App() {
       </div>
       <div>
         <p>
-        Please <a href="mailto:enquiries@crawfordcorry.com">contact us</a> for enquiries or download our brochure.
+        Please <a href="mailto:enquiries@crawfordcorry.com">contact us</a> for enquiries 
+        or <a href="/documents/Crawford_Corry_Brochure_2025_05.pdf" download>download</a> our 
+        brochure.
         </p>
       </div>
     </>
