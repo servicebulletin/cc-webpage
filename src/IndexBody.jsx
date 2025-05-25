154cf2d327d0861d447bc5c5ffca5ff4e0a2fdad
servicebulletin <36870168+servicebulletin@users.noreply.github.com>
2025-05-25 17:36:38 +0800

    Change text width

--- src/IndexBody.jsx
+++ src/IndexBody.jsx
@@ -1,19 +1,20 @@
 function IndexBody() {
 
   return (
-    <div className="index-body">
-      <p>
+    <div>
+    {/* <div className="index-body"> */}
+      <p className="index-p">
         We curate life-changing professional journeys in Vietnam that combine career 
         development, cultural discovery, and personal growth. Whether you're embarking on 
         a professional placement, gap year, or summer programme, our placements are 
         tailored to your goals and interests.
       </p>
-      <p>
+      <p className="index-p">
         Safety and privacy are our utmost priorities, particularly as we work with students 
         and young adults. In keeping with our commitment to exclusivity and trust, we 
         maintain a deliberately minimal public presence and refrain from social media activity. 
       </p>
-      <p>
+      <p className="index-p">
         Crawford & Corry was founded by two professionals with backgrounds at Eton and Oxford, 
         combined with extensive experience in Vietnam.
       </p>
