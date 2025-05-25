ef15410e741ac152370a40d24f880661def2cb49
servicebulletin <36870168+servicebulletin@users.noreply.github.com>
2025-05-25 15:28:37 +0800

    Change header formatting

--- src/IndexHeader.jsx
+++ src/IndexHeader.jsx
@@ -1,8 +1,14 @@
+import './index.css'
+
 function IndexHeader() {
 
   return (
-  <div>
-    <h1>CRAWFORD&CORRY</h1>
+  <div className="index-header">
+    <img 
+      src="src/assets/Crawford&Corry_logo.png" 
+      alt="Crawford & Corry" 
+      style={{ width: '100%', height: 'auto', display: 'block' }} 
+    />
     <h4>EMPLOYABLE | EDUCATIONAL | EXPERIENTIAL</h4>
   </div>
   )
