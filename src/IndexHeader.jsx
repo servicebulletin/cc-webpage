7ff412665a97eb4b23d74803ede57b0b0e4ffa8d
servicebulletin <36870168+servicebulletin@users.noreply.github.com>
2025-06-01 19:40:40 +0800

    Modify gitignore to remove dist

--- src/IndexHeader.jsx
+++ src/IndexHeader.jsx
@@ -5,8 +5,8 @@ function IndexHeader() {
   return (
   <div className="index-header">
     <img 
-      // src="./src/assets/CrawfordCorry_logo.png" 
-      src="./assets/CrawfordCorry_logo.png"
+      src="./src/assets/CrawfordCorry_logo.png" 
+      // src="./assets/CrawfordCorry_logo.png"
       alt="Crawford & Corry" 
       style={{ width: '100%', height: 'auto', display: 'block' }} 
     />
