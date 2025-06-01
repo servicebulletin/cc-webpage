22b0ce4d07dc879d2ce455ab4988ce83fcf73077
servicebulletin <36870168+servicebulletin@users.noreply.github.com>
2025-06-01 13:07:36 +0800

    Update IndexHeader.jsx

--- src/IndexHeader.jsx
+++ src/IndexHeader.jsx
@@ -5,7 +5,8 @@ function IndexHeader() {
   return (
   <div className="index-header">
     <img 
-      src="src/assets/Crawford&Corry_logo.png" 
+      src="src/assets/C&C_logo.png"
+      // src="src/assets/Crawford&Corry_logo.png" 
       alt="Crawford & Corry" 
       style={{ width: '100%', height: 'auto', display: 'block' }} 
     />
